a5512c5f9d2d3f4cf4f50a32a5017d8c744210b9
Agnė <agne3969@gmail.com>
2020-06-22 13:11:16 +0300

    Add project files.

---
+++ shortest-path-array/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace shortest_path_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
